65b43bde9ce520f11c8c4a4699444d97fbebe84d
JOMANA HELLO <j.hussein2327@nu.edu.eg>
2025-12-15 15:37:04 +0200

    Adjusted issue in duplicated code

--- StudentManagementSystem.DAL/Contracts/IUnitOfWork.cs
+++ StudentManagementSystem.DAL/Contracts/IUnitOfWork.cs
@@ -16,19 +16,9 @@ namespace StudentManagementSystem.DAL.Contracts
         IBaseRepository<StudentProfile> StudentProfiles { get; }
         IBaseRepository<AcademicPlan> AcademicPlans { get; }
 
-        public IBaseRepository<Course> Courses { get; }
         public IBaseRepository<Cart> Carts { get; }
         public IBaseRepository<CartItem> CartItems { get; }
-        public IBaseRepository<Room> Rooms { get; }
-        public IBaseRepository<TimeSlot> TimeSlots { get; }
-        public IBaseRepository<Section> Sections { get; }
-        public IBaseRepository<ScheduleSlot> ScheduleSlots { get; }
-        //public IBaseRepository<Transcript> Transcripts { get; }
-        public IBaseRepository<Enrollment> Enrollments { get; }
-        public IBaseRepository<AdminProfile> AdminProfiles { get; }
-        public IBaseRepository<InstructorProfile> InstructorProfiles { get; }
-        public IBaseRepository<StudentProfile> StudentProfiles { get; }
-        public IBaseRepository<AcademicPlan> AcademicPlans { get; }
+        
         Task SaveChangesAsync();
     }
 }
